8eb38bfccbcb54e4576f3569d251b41a58c40077
Barbara Calas <barbara.calas@orange.fr>
2022-05-31 17:59:03 +0200

    page compétences

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,8 @@ class PagesController < ApplicationController
   end
 
   def competences
+    # @hard_skills = ["HTML5", "CSS3", "JavaScript ES6", "Bootstrap", "Ruby", "Ruby on Rails", "SQL", "PostgreSQL", "MVC", "Git", "Github", "OOP", "Heroku", "API", "UI/UX Design", "Figma", "Relational Database", "Photoshop"]
+    # @soft_skills = ["Esprit d'équipe", "Capacité d'adaptation", "Satisfaction clients", "Organisation", "Force de proposition", "Médiation", "Autonomie"]
   end
 
   def formations
